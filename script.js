cd209ad0dc2b9d3803d3dca2179be06906cfe992
vadimsusername <vadimburyakovsky@yahoo.com>
2020-02-14 22:34:34 -0500

    added script code to color code the textareas depending on the time

--- script.js
+++ script.js
@@ -1,19 +1,26 @@
-
+var currentHour = moment().toObject().hours;//
 
 $("textarea").each(function(){
     var dataHour = parseInt($(this).attr("data-hour"));
-    console.log(dataHour);
+    //console.log(dataHour);
     var entry = localStorage.getItem("hour"+ $(this).attr("data-hour"));
     if(entry){
         $(this).val(entry);
     }
     
+    if(currentHour === dataHour){
+        $(this).addClass("present");
+    }else if(dataHour < currentHour){
+        $(this).addClass("past");
+    }else if(dataHour > currentHour){
+        $(this).addClass("future");
+    }   
   
 })
 
 $("button").on("click",function(){
-    console.log($(this).parent().prev().val());
-    console.log($(this).parent().prev().attr("data-hour"));
+    //console.log($(this).parent().prev().val());
+    //console.log($(this).parent().prev().attr("data-hour"));
     localStorage.setItem("hour" + $(this).parent().prev().attr("data-hour"),$(this).parent().prev().val());
 
 })
